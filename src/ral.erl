c737976db800f24f8e1215cf463b5496f5595d1e
Keaton Smith <keaton.smith365@gmail.com>
2023-11-15 09:05:32 -0700

    Wednesday changes
    Co-authored-by: Aidan Stacey <aidh18@users.noreply.github.com>
    Co-authored-by: SterlingB9 <SterlingB9@users.noreply.github.com>
    Co-authored-by: BrennonL <BrennonL@users.noreply.github.com>
    Co-authored-by: chanyoung1999 <chanyoung1999@users.noreply.github.com>

--- src/ral.erl
+++ src/ral.erl
@@ -3,26 +3,80 @@
 %% API exports
 -export([test/0]).
 
-get(RA_list, Index) ->
-    todo.
+% This function ultimatly gets a value from the random access list. It has a
+% few helper functions. They are search, build_bin_list, and find.
+% Paramiters: R_A_list (ra list), Index (the value we are searching for)
+get([], Index) -> nil;
+get([{Element, nil, nil} | T], 0) ->
+	Element;
+get(R_A_list, Index) ->
+  {Skipped_indicies, Tree} = search(R_A_list, Index, 0)
+	if
+		Tree =:= nil ->
+			nil;
+		true ->
+			{Leaf_count, _, _} = Tree,
+			find(Tree, build_bin_list(Index - Skipped_indicies, Leaf_count))
+	end.
 
-search(RAL, Index, Acc_skipped) ->
-    todo.
+% search does stuff
+search([], Index, _) -> {fail, nil};
+search([H|T], 0, _) -> {0,H};
+search([{_, nil, nil}|T], Index, Accum) -> {};
+search([{Leaf_count, Next_l, Next_r} | T], Index, Accum) when Leaf_count < Index - Accum ->
+	search(T, Index, Accum + Leaf_count);
+search([H | T], Index, Accum) ->
+	{Accum, H}.
+
 
 build_bin_list(_,1) ->
-    pass
+    [];
 build_bin_list(N,Bit_space) ->
-    pass
+    Lesser_power = Bit_space / 2,
+		case N >= Lesser_power of
+			true ->
+				[1] ++ build_bin_list(N - Lesser_power, Lesser_power);
+			false ->
+				[0] ++ build_bin_list(N, Lesser_power)
+		end.
 
+% This finds the a node in the random access list
+% Paramiters: Element (Your search value), Next_l (left node), Next_r (right node).
 find(Tree, Traversal_list_binary) -> Value;
 find(nil, Traversal_list_binary) -> nil;
 find({Element, _, _}, []) -> Element;
-find({_, Next_l, _}, [0|T]) -> find(Next_l, T);
-find({_, Next_l, _}, [1|T]) -> find(Next_r, T).
+find({_, Next_l, _}, [0|T]) -> find(Next_l, T); 
+find({_, _, Next_r}, [1|T]) -> find(Next_r, T).
 
-update(Tree, Index, Value, Acc_skipped) ->
-    todo.
 
+update([], _, _, _) -> fail;
+update([{_, nil, nil}], 0, Value, _) -> [{Value, nil, nil}];
+update([{Leaf_value, nil, nil} | T], Index, Value, Accum) ->
+	[{Leaf_value, nil, nil}] ++ update(T, Index, Value, Accum+1);;
+update([{Leaf_count, Next_l, Next_r} | T], Index, Value, Accum) when Leaf_count < Index - Accum ->
+    [{Leaf_count, Next_l, Next_r}] ++ update(T, Index, Value, Accum + Leaf_count);
+update([H | T], Index, Value, Accum)->
+	{Leaf_count, _Next_l, _Next_r} = H,
+    [replace(H, (build_bin_list(Index - Accum, Leaf_count)))] ++ T.
+
+
+% This function inserts a node into the random access tree.
+% Paramiters: Leaf, List
+cons(nil, RAL)->
+	fail;
+cons(Leaf, nil)->
+	fail;
+cons({_, Next_l, Next_r}, _) when not is_atom(Next_l) or not is_atom(Next_r) ->
+	fail;
+cons(Leaf, []) ->
+	[Leaf];
+cons(Leaf, [{Value, nil, nil}|T]) ->
+	link({2, Leaf, {Value, nil, nil}, T});
+cons(Leaf, RAL) ->
+	Leaf ++ RAL.
+
+link(Tree, RAL) ->
+	todo.
 
 
 %%% Only include the eunit testing library and functions
@@ -31,13 +85,23 @@ update(Tree, Index, Value, Acc_skipped) ->
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
  
-RAL1 = [{6, nil, nil}, {2, {2, nil, nil}, {18, nil, nil}}, {4, {2, {5, nil, nil}, {9, nil, nil}}, {2, {2, nil, nil}, {13, nil, nil}}}],
-RAL2 = [{1, nil, nil}, {2, {4, nil, nil}, {2, nil, nil}}]
+% Insertion order 18, 6, 1, 5, 7, 3, 42, 13
+RAL0 = [],
+RAL1 = [{18, nil, nil}],
+RAL2 = [{2, {6, nil, nil}, {18, nil, nil}}],
+RAL3 = [{1, nil, nil}, {2, {6, nil, nil}, {18, nil, nil}}],
+RAL4 = [{4, {2, {5, nil, nil}, {1, nil, nil}}, {2, {6, nil, nil}, {18, nil, nil}}}],
+RAL5 = [{7, nil, nil}, {4, {2, {5, nil, nil}, {1, nil, nil}}, {2, {6, nil, nil}, {18, nil, nil}}}],
+RAL6 = [{2, {3, nil, nil}, {7, nil, nil}}, {4, {2, {5, nil, nil}, {1, nil, nil}}, {2, {6, nil, nil}, {18, nil, nil}}}],
+RAL7 = [{42, nil, nil}, {2, {3, nil, nil}, {7, nil, nil}}, {4, {2, {5, nil, nil}, {1, nil, nil}}, {2, {6, nil, nil}, {18, nil, nil}}}],
+RAL8 = [{8, {4, {2, {13, nil, nil}, {42, nil, nil}}, {2, {3, nil, nil}, {7, nil, nil}}}, {4, {2, {5, nil, nil}, {1, nil, nil}}, {2, {6, nil, nil}, {18, nil, nil}}}}],
 
 get_test_()->
-	[?_assertEqual(nil, get(nil)),%happy path
-    ?_assertEqual(13, get(1))
-    ?_assertEqual(, get())
+	[?_assertEqual(nil, get(RAL0, 7)),%happy path
+    ?_assertEqual(18, get(RAL1, 0)),
+    ?_assertEqual(18, get(RAL2, 1)),
+    ?_assertEqual(6, get(RAL3, 1)),
+    ?_assertEqual(1, get(RAL6, 3))
 	 %nasty thoughts start here
 	].
 
@@ -47,7 +111,11 @@ search_test_()->
 	].
 
 build_bin_list_test_()->
-	[?_assertEqual(nil, build_bin_list(nil)),%happy path
+	[?_assertEqual([0], build_bin_list(0, 2)),%happy path
+	?_assertEqual([1], build_bin_list(1, 2)),%happy path
+	?_assertEqual([0, 0], build_bin_list(0, 4)),%happy path
+	?_assertEqual([1, 0], build_bin_list(2, 4)),%happy path
+	?_assertEqual([0, 1, 1, 0], build_bin_list(6, 16))%happy path
 	 %nasty thoughts start here
 	].
 
@@ -57,7 +125,8 @@ find_test_()->
 	].
 
 update_test_()->
-	[?_assertEqual(nil, update(nil)),%happy path
+	[?_assertEqual([{7, nil, nil}], update(RAL1, 0, 7, 0)),%happy path
+	?_assertEqual([{{6, nil, nil}, {7, nil, nil}}], update(RAL2, 0, 7, 0))%happy path
 	 %nasty thoughts start here
 	].
 
@@ -67,8 +136,18 @@ replace_test_()->
 	].
 
 cons_test_()->
-	[?_assertEqual(nil, cons(nil)),%happy path
+	[?_assertEqual(RAL1, cons({18, nil, nil}, RAL0)),%happy path
+	 ?_assertEqual(RAL2, cons({6, nil, nil}, RAL1)),%happy path
+	 ?_assertEqual(RAL3, cons({1, nil, nil}, RAL2)),%happy path
+	 ?_assertEqual(RAL4, cons({5, nil, nil}, RAL3)),%happy path
+	 ?_assertEqual(RAL5, cons({7, nil, nil}, RAL4)),%happy path
+	 ?_assertEqual(RAL6, cons({3, nil, nil}, RAL5)),%happy path
+	 ?_assertEqual(RAL7, cons({42, nil, nil}, RAL6)),%happy path
+	 ?_assertEqual(RAL8, cons({13, nil, nil}, RAL7)),%happy path
 	 %nasty thoughts start here
+	 ?_assertEqual(nil, cons(nil, nil)),
+	 ?_assertEqual(RAL1, cons(nil, RAL1)),
+	 ?_assertEqual(nil, cons({18, nil, nil}, nil))
 	].
 
 link_test_()->
